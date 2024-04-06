d927743044dac6582a2e20dbbb3053e1f01b3b1d
Loice-Staice <staiceloicea@gmail.com>
2024-04-06 20:04:00 +0300

    add files

--- control.js
+++ control.js
@@ -1,18 +1,45 @@
 //1
+const arrays=[17,23,45,34,3,67,90,57,56,73]
+function items(arrays){
+    arrays.forEach((item,element )=> {
+        if(index<4){
+            items[index] = items*10;
+        }
+        else if (index>=items.length[-2]){
+            items[index]=item +5
+        }
+        
+    });
+    console.log(items);
+}
+//2
 function manipulate(){
-    let number=[1,2,3,4,5,6]
-    for(var i=0;i<number.length;i++){
+    let arrayNums=[1,2,3,4,5,6,7,8,9]
+    while(arrayNums[4]){
+        console.log(arrayNums)
+        break
+    }
         
     }
-
-}
-//04
-function stringArray(strArray){
-    for (let i=0;i <strArray.length; i++){
-
-        console.log(strArray[i]);
+//3
+function thisFruit(arr){
+    for (let i=0; i<arr.length; i ++){
+        if (i===1){
+         
+            continue;
+        }
+        console.log(arr[i]);
     }
 }
+//04
+function stringArr(arr){
+    let names= ["Luby","Shama","Yar","Staicey"]
+    stringArr(names);
+    for (let i = 0; i<arr.length;i++){
+        console.log('names; ${arr[i]}');
+    }
+}
+
 
 //5
 function reverseString(word){
